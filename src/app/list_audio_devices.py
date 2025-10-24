325fc78cbf9a1df097d11fd74397df6e62ac8012
Daniel Collier <daniel.collier@luizalabs.com>
2025-10-23 22:03:20 -0300

    feat: list audio devices

---
+++ src/app/list_audio_devices.py
@@ -0,0 +1,22 @@
+"""
+Utility script to list all available audio input devices (microphones)
+detected by the sounddevice library on the system.
+
+Author: Daniel Collier
+GitHub: https://github.com/danielfcollier
+Year: 2025
+"""
+
+from src.library.audio_device.selector import AudioDeviceSelector
+
+
+def main():
+    """
+    The main function of the script.
+    It directly calls the static method to display the audio devices.
+    """
+    AudioDeviceSelector.show_audio_devices()
+
+
+if __name__ == "__main__":
+    main()
