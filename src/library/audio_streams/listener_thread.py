bb5372ed16bb400a27f7f97463b3536b4d6720f5
Daniel Collier <daniel.collier@luizalabs.com>
2025-10-28 08:47:27 -0300

    refactor: rename and remove variables

--- src/library/audio_streams/listener_thread.py
+++ src/library/audio_streams/listener_thread.py
@@ -3,7 +3,7 @@ Implements the audio stream listener (producer) thread.
 
 This module contains the class responsible for opening an audio input stream,
 continuously reading audio chunks from the microphone, and placing them onto
-a shared queue for consumption by other threads.
+a queue for consumption by other threads.
 
 Author: Daniel Collier
 GitHub: https://github.com/danielfcollier
@@ -16,7 +16,7 @@ import sounddevice as sd
 import logging
 
 from src.library.audio_device.config import AudioDeviceConfig
-from src.library.datetime_stamp import DatetimeStamp
+from src.datetime_stamp import DatetimeStamp
 
 logger = logging.getLogger(__name__)
 
@@ -27,16 +27,13 @@ class AudioStreamsListenerThread:
 
     This class acts as the "Producer" in a producer-consumer pattern. Its sole
     responsibility is to continuously read audio chunks from the sound device
-    using `sounddevice.InputStream` and place them onto a shared queue for
-    further processing by other threads (Consumers).
+    using `sounddevice.InputStream` and place them onto a queue for further
+    processing by other threads (Consumers).
 
-    It runs until a `stop_event` is set, ensuring graceful shutdown. It uses
-    high priority to minimize the risk of audio buffer overflows.
+    It runs until a `stop_event` is set, ensuring graceful shutdown.
     """
 
-    def __init__(
-        self, audio_device_config: AudioDeviceConfig, shared_audio_queue: queue.Queue, stop_event: threading.Event
-    ):
+    def __init__(self, audio_device_config: AudioDeviceConfig, audio_queue: queue.Queue, stop_event: threading.Event):
         """
         Initializes the audio listener thread.
 
@@ -44,16 +41,15 @@ class AudioStreamsListenerThread:
                                     audio stream (e.g., sample rate, block size,
                                     device ID, dtype). This configuration dictates how
                                     the audio stream will be opened.
-        :param shared_audio_queue: A thread-safe `queue.Queue` instance. Raw audio
-                                   chunks captured from the microphone will be put
-                                   onto this queue.
+        :param audio_queue: A thread-safe `queue.Queue` instance. Raw audio chunks
+                                    captured from the microphone will be put onto this queue.
         :param stop_event: A `threading.Event` object used to signal the thread
                            to terminate its loop and exit gracefully. This event
                            is typically set by the main application thread upon
                            receiving a shutdown signal (SIGINT/SIGTERM).
         """
         self._audio_device_config = audio_device_config
-        self._queue = shared_audio_queue
+        self._queue = audio_queue
         self._stop_event = stop_event
 
         self._class_name = self.__class__.__name__
@@ -64,11 +60,10 @@ class AudioStreamsListenerThread:
         The main execution loop for the audio listener thread.
 
         Continuously reads audio chunks from the configured input device via
-        `sounddevice.InputStream` and puts them onto the shared queue as tuples
-        containing the audio data (numpy array) and a timestamp.
+        `sounddevice.InputStream` and puts them onto the queue as tuples containing
+        the audio data (numpy array) and a timestamp.
 
-        This thread prioritizes reading from the audio buffer to prevent overflows
-        and performs minimal processing. It runs until the `stop_event` is set.
+        It runs until the `stop_event` is set.
         """
         logger.info(f"{self._class_name} thread started.")
 
@@ -84,7 +79,6 @@ class AudioStreamsListenerThread:
                 samplerate=sample_rate,
                 dtype=dtype,
                 channels=1,
-                high_priority=self._audio_device_config.high_priority,
             ) as stream:
                 logger.debug(
                     f"Opened audio stream on device ID {device_id} with SR={sample_rate}, Blocksize={block_size}."
