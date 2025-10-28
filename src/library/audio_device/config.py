03c900f3c2b83bce7646f8d8dd545ddb9cd3eefa
Daniel Collier <danielfcollier@gmail.com>
2025-10-28 08:49:18 -0300

    feat: add config native rate

--- src/library/audio_device/config.py
+++ src/library/audio_device/config.py
@@ -26,8 +26,8 @@ class AudioDeviceConfig:
         target_audio_device: AudioDeviceSelector,
         sample_rate: float,
         buffer_seconds: float,
-        high_priority: bool = False,
         dtype: str = "float32",
+        high_priority: bool = False,
     ):
         """
         Initializes the audio device configuration object.
@@ -39,22 +39,23 @@ class AudioDeviceConfig:
                             (e.g., 48000.0, 16000.0).
         :param buffer_seconds: The desired duration of each audio buffer (chunk) in seconds.
                                This affects latency and processing granularity.
+        :param dtype: The desired data type for the audio samples (e.g., 'float32', 'int16').
+                      'float32' (range -1.0 to 1.0) is often preferred for processing.
+                      Defaults to 'float32'.
         :param high_priority: A boolean flag indicating whether to request high-priority
                               processing for the audio stream from the operating system.
                               Crucial for real-time applications to prevent buffer overflows.
                               Defaults to False.
-        :param dtype: The desired data type for the audio samples (e.g., 'float32', 'int16').
-                      'float32' (range -1.0 to 1.0) is often preferred for processing.
-                      Defaults to 'float32'.
         """
         self.id = target_audio_device.id
+        self.native_rate = target_audio_device.native_rate
         self.sample_rate = sample_rate
         self.buffer_seconds = buffer_seconds
-        self.dtype = dtype
         self.block_size = int(sample_rate * buffer_seconds)
+        self.dtype = dtype
         self.high_priority = high_priority
 
         print(
-            f"AudioDeviceConfig initialized: Device ID={self.id}, SR={self.sample_rate}, "
-            f"Blocksize={self.block_size}, High Priority={self.high_priority}, Dtype={self.dtype}"
+            f"AudioDeviceConfig initialized: Device ID={self.id}, SR={self.sample_rate}Hz,"
+            f"NR={self.native_rate}Hz Blocksize={self.block_size}, Dtype={self.dtype}"
         )
